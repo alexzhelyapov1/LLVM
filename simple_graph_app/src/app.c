d4cd7d9eb5debbe37dd969ad519ac46c03813821
alex_ubuntu24_blue <ubuntu24_blue@mail.com>
2024-09-19 22:17:10 +0300

    maybe

--- simple_graph_app/src/app.c
+++ simple_graph_app/src/app.c
@@ -7,17 +7,17 @@ void init_matrix(unsigned int matrix[SIM_X_SIZE][SIM_Y_SIZE])
 	{
 		for (int x = 0; x < SIM_X_SIZE; ++x)
 		{
-			if (x < 10)
+			if (x < 10 || y < 10)
 			{
 				matrix[x][y] = 0xFF00FF00;
 			}
-            else if (x > 150 && x < 160) {
+            else if (x > 150 && x < 160 || y > 150 && y < 160) {
                 matrix[x][y] = 0xFFFF0000;
             }
-            else if (x > 300 && x < 310) {
+            else if (x > 300 && x < 310 || y > 300 && y < 310) {
                 matrix[x][y] = 0xFF0000FF;
             }
-            else if (x > 450 && x < 460) {
+            else if (x > 450 && x < 460 || y > 450 && y < 460) {
                 matrix[x][y] = 0xFFFFFFFF;
             }
 			else
