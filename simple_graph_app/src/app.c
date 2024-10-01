e6b6e7daa88f184a7d7e6d7bd40a7893a4bdf96e
alex_ubuntu24_blue <ubuntu24_blue@mail.com>
2024-10-01 20:22:48 +0300

    removed 2-dim arrays

--- simple_graph_app/src/app.c
+++ simple_graph_app/src/app.c
@@ -1,6 +1,6 @@
 #include "sim.h"
 
-void initMatrix(unsigned int matrix[SIM_X_SIZE][SIM_Y_SIZE])
+void initMatrix(unsigned int matrix[SIM_X_SIZE * SIM_Y_SIZE])
 {
 	for (int y = 0; y < SIM_Y_SIZE; ++y)
 	{
@@ -8,42 +8,42 @@ void initMatrix(unsigned int matrix[SIM_X_SIZE][SIM_Y_SIZE])
 		{
 			if (x < 10 || y < 10)
 			{
-				matrix[x][y] = 0xFF00FF00;
+				matrix[x + y * SIM_X_SIZE] = 0xFF00FF00;
 			}
             else if (x > SIM_X_SIZE / 4 * 1 && x < SIM_X_SIZE / 4 * 1 + 10) {
-                matrix[x][y] = 0xFFFF0000;
+                matrix[x + y * SIM_X_SIZE] = 0xFFFF0000;
             }
             else if (x > SIM_X_SIZE / 4 * 2 && x < SIM_X_SIZE / 4 * 2 + 10) {
-                matrix[x][y] = 0xFF0000FF;
+                matrix[x + y * SIM_X_SIZE] = 0xFF0000FF;
             }
             else if (x > SIM_X_SIZE / 4 * 3 && x < SIM_X_SIZE / 4 * 3 + 10) {
-                matrix[x][y] = 0xFFFFFFFF;
+                matrix[x + y * SIM_X_SIZE] = 0xFFFFFFFF;
             }
 			else
 			{
-				matrix[x][y] = 0xFF000000;
+				matrix[x + y * SIM_X_SIZE] = 0xFF000000;
 			}
 		}
 	}
 }
 
-void updateMatrix(unsigned int matrix[SIM_X_SIZE][SIM_Y_SIZE])
+void updateMatrix(unsigned int matrix[SIM_X_SIZE * SIM_Y_SIZE])
 {
 	for (int y = 0; y < SIM_Y_SIZE - 1; ++y)
 	{
 		for (int x = SIM_X_SIZE - 1; x > 0; --x)
 		{
-			matrix[x][y] = matrix[x - 1][y] / 4 + matrix[x][y] / 4 + matrix[x][y + 1] / 4 + matrix[x - 1][y + 1] / 4;
+			matrix[x + y * SIM_X_SIZE] = matrix[x - 1 + y * SIM_X_SIZE] / 4 + matrix[x + y * SIM_X_SIZE] / 4 + matrix[x + (y + 1) * SIM_X_SIZE] / 4 + matrix[x - 1 + (y + 1) * SIM_X_SIZE] / 4;
 		}
 	}
 }
 
-void drawMatrix(unsigned int matrix[SIM_X_SIZE][SIM_Y_SIZE])
+void drawMatrix(unsigned int matrix[SIM_X_SIZE * SIM_Y_SIZE])
 {
 	for (int y = 0; y < SIM_Y_SIZE; ++y)
 		for (int x = 0; x < SIM_X_SIZE; ++x)
 		{
-			simPutPixel(x, y, matrix[x][y]);
+			simPutPixel(x, y, matrix[x + y * SIM_X_SIZE]);
 		}
 
 	simFlush();
@@ -51,7 +51,7 @@ void drawMatrix(unsigned int matrix[SIM_X_SIZE][SIM_Y_SIZE])
 
 void app()
 {
-	unsigned int matrix[SIM_X_SIZE][SIM_Y_SIZE];
+	unsigned int matrix[SIM_X_SIZE * SIM_Y_SIZE];
 	initMatrix(matrix);
 
 	for (int step = 0; step < 1000; ++step)
